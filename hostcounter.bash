b627349f1e810bd74beefb8dc8b9171f0f044282
root <root@cf-domain.it>
2020-12-22 10:09:05 +0100

    bug fix

--- hostcounter.bash
+++ hostcounter.bash
@@ -3,7 +3,7 @@ COUNTER=0
 for ip in 192.168.178.{1..254}; do
   ping -c 1 -W 1 -t 2 -s 10 $ip > /dev/null 2> /dev/null;
   if [ $? -eq 0 ]; then
-    echo "${ip} is up";
+    #echo "${ip} is up";
     #arp -n -i eth0 $ip | grep ether >> /dev/null
     let COUNTER++
   fi
